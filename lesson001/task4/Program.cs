4f25e28edc1a44de85a589786386ef8c3edc8e2e
dreamvik <ideasea@yandex.ru>
2023-02-03 13:25:57 +0400

    task4

---
+++ lesson001/task4/Program.cs
@@ -0,0 +1,26 @@
+﻿//функция, определяющая четность числа
+bool isOrder(int Number){
+  int remainder = Number%2;
+  return remainder == 0;
+}
+//1 запросить у пользователя конечное число и сохранить его в переменную
+System.Console.WriteLine("Введите последнее число");
+string valueString = Console.ReadLine();
+//перевести число в int
+int N = Convert.ToInt32(valueString);
+//для каждого числа от 1 до N проверяем четность и в случае, если оно четное, добавляем к строке
+string Numbers = "";
+int count = 1;
+string separator = "";
+while (count <= N)
+{
+    if (count > 2){
+        separator = ", ";
+    }
+    if (isOrder(count)){
+        Numbers = Numbers + separator + count;
+    }
+    count++;
+}
+
+System.Console.WriteLine($"Четные числа: {Numbers}");
