9b67965b6868fcef4bcba55dc910571d39835a17
dreamvik <ideasea@yandex.ru>
2023-02-03 13:22:31 +0400

    task3

---
+++ lesson001/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿//1 запросить у пользователя число и сохранить его в переменную
+System.Console.WriteLine("Введите число");
+string valueString = Console.ReadLine();
+//перевести числj в int
+int number = Convert.ToInt32(valueString);
+//получить остаток от деления на 2
+int remainder = number%2;
+//если значение остатка равно 0, то число четное
+string NumberType = "нечетное";
+if(remainder == 0){
+    NumberType = "четное";
+}
+
+System.Console.WriteLine($"Число {NumberType}!");
+
